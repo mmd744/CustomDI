a0d506fb72da67d8f24874cadee8ceedb3ecd70e
Teymur Huseynov <teymur.huseynov@kibrit.tech>
2021-12-03 16:22:43 +0400

    implement thread-safety using double-check locking

--- DIDemo/DIContainer.cs
+++ DIDemo/DIContainer.cs
@@ -1,22 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DIDemo
+﻿namespace DIDemo
 {
     public class DIContainer
     {
         private static DIContainer _instance = null;
+        private static readonly object padlock = new object();
         private DIContainer() { }
-        public static DIContainer GenerateContainer()
-        {
-            if (_instance != null)
-                return _instance;
+        //public static DIContainer GenerateContainer()
+        //{
+        //    if (_instance != null)
+        //        return _instance;
 
-            else
+        //    else
+        //    {
+        //        var newContainer = new DIContainer();
+        //        _instance = newContainer;
+        //        return _instance;
+        //    }
+        //}
+        public static DIContainer Instance
+        {
+            get
             {
-                var newContainer = new DIContainer();
-                _instance = newContainer;
+                if (_instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DIContainer();
+                        }
+                    }
+                }
                 return _instance;
             }
         }
